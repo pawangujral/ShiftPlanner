f0d435f6c745916644e688fdb1ed0e7c3318aafa
Pawan Gujral <gujral87@gmail.com>
2022-08-07 12:27:42 +0200

    refactor: 💡 update

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import sass from 'rollup-plugin-sass';
 import typescript from 'rollup-plugin-typescript2';
 import image from '@rollup/plugin-image';
+import babel from '@rollup/plugin-babel';
 
 import pkg from './package.json';
 
@@ -15,6 +16,6 @@ export default {
       strict: false,
     },
   ],
-  plugins: [sass({ insert: true }), typescript(), image()],
+  plugins: [sass({ insert: true }), typescript(), image(), babel({ babelHelpers: 'bundled' })],
   external: ['react', 'react-dom'],
 };
