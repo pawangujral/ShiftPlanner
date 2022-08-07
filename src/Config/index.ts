1e1239705b347fc8de39f36abdde5ea74fac040e
Pawan Gujral <gujral87@gmail.com>
2022-08-07 16:19:37 +0200

    fix: 🐛 style

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -1,1 +1,21 @@
-export * from "./State";
+import { IDefaultState } from '../Utils';
+
+const DEFAULT_STATE: IDefaultState = {
+  offset: 600,
+  condensed: false,
+  gridColumnCount: 25, //! this should be dynamic
+  gridRowSize: {
+    max: 75,
+    width: 60,
+  },
+  zoom: {
+    default: 1,
+    min: 1,
+    max: 8,
+  },
+  time: {
+    format: 24,
+  },
+};
+
+export default DEFAULT_STATE;
